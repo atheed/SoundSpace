2fd22833a1da6eedcc26ee6f996015b279780a01
Trevor Anderson <t.anderson@mail.utoronto.ca>
2015-10-31 16:51:00 -0400

    Trying to get an on end event handler to work for the audio-player

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 $(window).ready(function() {
     var fileInput = document.getElementById("FileInput");
+    var src;
     fileInput.addEventListener('change', function(evt){
-        var file = fileInput.files[0];
+        var i = 0;
+        var files = fileInput.files;
+        var file = files[i];
         var reader = new FileReader();
         reader.onload = (function(theFile) {
             return function(e) {
+                src = e.target.result.toString();
                 $("audio").remove();
-                $("body").append("<audio controls></audio>");
-                $("audio").append("<source id='player' src='" + e.target.result.toString()+"' type='audio/mp3'>");
+                $("body").append("<audio id='player' preload='buffer' autoplay controls></audio>");
+                $("audio").append("<source id='src' src='" + src +"' type='audio/mp3'>");
                 $("audio").append("Your browser does not support this music player.");
+                document.getElementById("player").onended = onEnd();
             };
         })(file);
         reader.readAsDataURL(file);
     });
 });
+
+function onEnd() {
+    alert("This should come up when the song is over");
+    /*$("audio").remove();
+    $("body").append("<audio controls></audio>");
+    $("audio").append("<source id='player' src='" + src +"' type='audio/mp3'>");
+    $("audio").append("Your browser does not support this music player.");
+    $("audio").bind("ended", onEnd());*/
+    //This code was supposed to just re-add the same song again.
+}
